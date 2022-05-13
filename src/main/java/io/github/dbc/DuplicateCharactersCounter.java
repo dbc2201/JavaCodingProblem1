20ea6eb9920a561d92eb69985e61c5b035ea7a55
dbc2201 <divyanshb22@gmail.com>
2022-05-13 23:30:43 +0530

    add the starter code

---
+++ src/main/java/io/github/dbc/DuplicateCharactersCounter.java
@@ -0,0 +1,15 @@
+package io.github.dbc;
+
+import java.util.Map;
+
+public class DuplicateCharactersCounter {
+    /**
+     * Counts the number of duplicate characters in a string.
+     *
+     * @param string String to count duplicate characters.
+     * @return Map with characters and their count.
+     */
+    public Map<Character, Integer> countDuplicateCharacters(String string) {
+        return Map.of();
+    }
+}
