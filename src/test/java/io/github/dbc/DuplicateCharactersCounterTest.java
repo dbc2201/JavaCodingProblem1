32c40656913d5be2adbea3335cbadb502b48dbc0
dbc2201 <divyanshb22@gmail.com>
2022-05-13 23:33:09 +0530

    add the test class

---
+++ src/test/java/io/github/dbc/DuplicateCharactersCounterTest.java
@@ -0,0 +1,22 @@
+package io.github.dbc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DuplicateCharactersCounterTest {
+
+    private DuplicateCharactersCounter counter;
+
+    @BeforeEach
+    void setUp() {
+        counter = new DuplicateCharactersCounter();
+    }
+
+    @Test
+    @DisplayName("")
+    void countDuplicateCharacters() {
+        Assertions.fail();
+    }
+}
