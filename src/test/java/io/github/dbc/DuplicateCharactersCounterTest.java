89f5fedabb1b233beed2631a22b703bed1d953ef
dbc2201 <divyanshb22@gmail.com>
2022-05-14 00:05:31 +0530

    add test case 1

--- src/test/java/io/github/dbc/DuplicateCharactersCounterTest.java
+++ src/test/java/io/github/dbc/DuplicateCharactersCounterTest.java
@@ -1,9 +1,10 @@
 package io.github.dbc;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DuplicateCharactersCounterTest {
     private DuplicateCharactersCounter counter;
@@ -14,8 +15,15 @@ class DuplicateCharactersCounterTest {
     }
 
     @Test
-    @DisplayName("")
-    void countDuplicateCharacters() {
-        Assertions.fail();
+    @DisplayName("should return count as 1 for all non duplicate characters")
+    void shouldReturnCount1ForNonDuplicateCharacters() {
+        // Arrange
+        String input = "abcdefghijklmnopqrstuvwxyz";
+        // Act
+        var characterIntegerMap = counter.countDuplicateCharacters(input);
+        // get the value for key 'a'
+        var count = characterIntegerMap.get('a');
+        // Assert
+        assertEquals(1, count, "the count value for all non duplicate characters should be 1");
     }
 }
