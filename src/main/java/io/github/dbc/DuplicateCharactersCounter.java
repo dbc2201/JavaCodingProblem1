6e863f77d68c30f04c9bae9f5afc7215a2f9df57
Ritik Bhushan Yadav <ritikbhushanyadav@gmail.com>
2022-05-14 01:12:16 +0530

    Update DuplicateCharactersCounter.java

--- src/main/java/io/github/dbc/DuplicateCharactersCounter.java
+++ src/main/java/io/github/dbc/DuplicateCharactersCounter.java
@@ -10,6 +10,18 @@ public class DuplicateCharactersCounter {
      * @return Map with characters and their count.
      */
     public Map<Character, Integer> countDuplicateCharacters(String string) {
-        return Map.of();
+        Map<Character,Integer> map = new HashMap<>();
+        var len = string.length();
+        for(var i = 0;i<len;i++)
+        {
+            var key = string.charAt(i);
+            if(map.containsKey(key))
+            {
+                map.put(key,map.get(key)+1);
+            }else{
+                map.put(key,1);
+            }
+        }
+        return map;
     }
 }
