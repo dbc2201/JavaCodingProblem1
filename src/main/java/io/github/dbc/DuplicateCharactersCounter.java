3a84ff3aac9bbeac44391f52a2ec8fde8dcdcf20
dbc2201 <divyanshb22@gmail.com>
2022-05-26 12:13:04 +0530

    add throws statement in javadoc

--- src/main/java/io/github/dbc/DuplicateCharactersCounter.java
+++ src/main/java/io/github/dbc/DuplicateCharactersCounter.java
@@ -8,6 +8,7 @@ public class DuplicateCharactersCounter {
      *
      * @param string String to count duplicate characters.
      * @return Map with characters and their count.
+     * @throws IllegalArgumentException if the input is null or empty.
      */
     public Map<Character, Integer> countDuplicateCharacters(String string) {
         return Map.of();
